0ec47baa8e75bb7fadd6784692e804a77a5d891c
Brandon <bmoore320@yahoo.com>
2020-05-06 19:14:31 -0700

    End of Section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -74,14 +74,9 @@ namespace DatingApp.API.Controllers
             return Ok(new {
                 token = tokenHandler.WriteToken(token)
             });
-
-
-        }
+        } 
     }
 }
-
-
-
 
 
 

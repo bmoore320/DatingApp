7d236d2b6433efc2008514fd28ba87fa3edf1ee2
Brandon <bmoore320@yahoo.com>
2023-02-09 00:55:21 -0800

    added client app

---
+++ API/Data/Migrations/20230201074343_AddedUserNametoUserTable_01-31-2023_11-43PM.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserNametoUserTable013120231143PM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+        }
+    }
+}
